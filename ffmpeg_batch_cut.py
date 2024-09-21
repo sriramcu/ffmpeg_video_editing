0e3d43cb0b491b171a8b02724c8fdf78905aaed1
sriramcu <csriram12345@gmail.com>
2024-09-21 23:15:46 +0530

    Made the program more modular

--- ffmpeg_batch_cut.py
+++ ffmpeg_batch_cut.py
@@ -8,7 +8,7 @@ from moviepy.editor import VideoFileClip
 import utils
 
 
-def segment_reverser(cut_out_segments, video_duration):
+def check_segment_correctness(cut_out_segments, video_duration):
     correctness_checker = []
     for seg in cut_out_segments:
         correctness_checker.append(seg[0])
@@ -18,9 +18,13 @@ def segment_reverser(cut_out_segments, video_duration):
             correctness_checker) != len(set(correctness_checker)):
         # Checks if the segments are in the correct order, the highest segment is less than the duration of the video,
         # and that there are no duplicates
-        print("Improper cut out segments")
-        sys.exit(-1)
+        raise ValueError("Improper cut out segments")
 
+
+def segment_reverser(cut_out_segments, video_duration):
+    """
+    Converts segments cut out from the video to segments of the video to be preserved
+    """
     reversed_segments = []
     position = 0
     if cut_out_segments[0][0] == 0:
@@ -34,48 +38,26 @@ def segment_reverser(cut_out_segments, video_duration):
             reversed_segments.append([cut_out_segments[i][1], cut_out_segments[i + 1][0]])
         else:
             reversed_segments.append([cut_out_segments[i][1], video_duration])
-
         position = reversed_segments[-1][-1]
-
     return reversed_segments
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Cut out segments from a video file')
-    parser.add_argument('-i', '--input_file', type=str, help='Input video file', required=True)
-    parser.add_argument('-s', '--segments', type=str, nargs='+', help='Segments to cut out in the format \"0:10-1:05\"', required=False)
-    parser.add_argument('-ss', '--segments_seconds', type=str, nargs='+', help='Segments to cut out in the format \"10-65\"', required=False)
-    parser.add_argument('-o', '--output_file', type=str, help='Output video file location', required=False)
-    args = parser.parse_args()
-    flag1 = False
-    flag2 = False
-    segments = []
-    if args.segments:
-        flag1 = True
-        segments = utils.time_intervals_converter(args.segments)
-    if args.segments_seconds:
-        flag2 = True
-        segments = args.segments_seconds
-
-    if flag1 == flag2 or segments == []:
-        raise ValueError("Exactly one of -s or -ss must be specified")
-
-    segments = [list(map(int, seg.split('-'))) for seg in segments]
-    print(segments)
+def ffmpeg_batch_cut(segments, args_input_file, args_output_file):
     debugger_file_name = os.path.join("generated_text_files", "debugger_file.txt")
     debugger_file = open(debugger_file_name, 'w')
-    input_video_file = f'"{args.input_file}"' # encloses input file in double quotes
+    input_video_file = f'"{args_input_file}"'  # encloses input file in double quotes
     full_ffmpeg_command = f'ffmpeg -i {input_video_file} -c copy'
     # initial variable that will be appended to during the for loop iterating over the reversed segments
 
     input_video_extension = input_video_file.split('.')[-1][:-1]  # without the '.'
-    if not args.output_file:
+    if not args_output_file:
         final_output_file = f"final_output.{input_video_extension}"
     else:
-        final_output_file = args.output_file
+        final_output_file = args_output_file
 
-    clip = VideoFileClip(args.input_file)
+    clip = VideoFileClip(args_input_file)
     video_duration = int(clip.duration) + 1
+    check_segment_correctness(segments, video_duration)
     segments = segment_reverser(segments, video_duration)
     debugger_file.write(str(segments))
 
@@ -105,6 +87,31 @@ def main():
     for interim_video_file in interim_files_list:
         os.remove(interim_video_file)
     os.remove(interim_videos_text_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Cut out segments from a video file')
+    parser.add_argument('-i', '--input_file', type=str, help='Input video file', required=True)
+    parser.add_argument('-s', '--segments', type=str, nargs='+', help='Segments to cut out in the format \"0:10-1:05\"', required=False)
+    parser.add_argument('-ss', '--segments_seconds', type=str, nargs='+', help='Segments to cut out in the format \"10-65\"', required=False)
+    parser.add_argument('-o', '--output_file', type=str, help='Output video file location', required=False)
+    args = parser.parse_args()
+    flag1 = False
+    flag2 = False
+    segments = []
+    if args.segments:
+        flag1 = True
+        segments = utils.time_intervals_converter(args.segments)
+    if args.segments_seconds:
+        flag2 = True
+        segments = args.segments_seconds
+
+    if flag1 == flag2 or segments == []:
+        raise ValueError("Exactly one of -s or -ss must be specified")
+
+    segments = [list(map(int, seg.split('-'))) for seg in segments]
+    print(segments)
+    ffmpeg_batch_cut(segments, args.input_file, args.output_file)
 
 
 if __name__ == '__main__':
